ee00948f7c8fd19e393d032c74facc63175e3b34
AnamulHaqueN <rafaielanam@gmail.com>
2025-10-06 12:15:21 +0600

    tailwind css is added

---
+++ Project/react-typescript-taskify/src/App.tsx
@@ -0,0 +1,14 @@
+
+import type { JSX } from 'react/jsx-runtime'
+import './App.css'
+
+function App(): JSX.Element {
+
+  return (
+    <div className="bg-blue-900 text-white p-4 rounded-lg">
+      Tailwind is working! 🎉
+    </div>
+  )
+}
+
+export default App
